45243bbe0f5fc9de4138083ef4cc92941e701314
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-09-02 05:15:38 +0530

    ADD: Dns header fields and reading header data from buffer
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -1,2 +1,97 @@
 package dns
 
+import (
+	"github.com/sadityakumar9211/go-res/pkg/bytepacketbuffer"
+)
+
+// ResultCode represents DNS result codes.
+type ResultCode int
+
+const (
+	NOERROR  ResultCode = 0
+	FORMERR  ResultCode = 1
+	SERVFAIL ResultCode = 2
+	NXDOMAIN ResultCode = 3
+	NOTIMP   ResultCode = 4
+	REFUSED  ResultCode = 5
+)
+
+// DnsHeader represents DNS packet header.
+type DnsHeader struct {
+	ID                   uint16
+	RecursionDesired     bool
+	TruncatedMessage     bool
+	AuthoritativeAnswer  bool
+	Opcode               uint8
+	Response             bool
+	ResultCode           ResultCode
+	CheckingDisabled     bool
+	AuthedData           bool
+	Z                    bool
+	RecursionAvailable   bool
+	Questions            uint16
+	Answers              uint16
+	AuthoritativeEntries uint16
+	ResourceEntries      uint16
+}
+
+// NewDnsHeader creates a new DNS header with default values.
+func NewDnsHeader() *DnsHeader {
+	return &DnsHeader{}
+}
+
+// Read reads DNS header data from the buffer.
+func (h *DnsHeader) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
+	id, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	h.ID = id
+
+	flags, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	a := byte(flags >> 8)
+	b := byte(flags & 0xFF)
+
+	h.RecursionDesired = (a & 1) > 0
+	h.TruncatedMessage = (a & 2) > 0
+	h.AuthoritativeAnswer = (a & 4) > 0
+	h.Opcode = (a >> 3) & 0x0F
+	h.Response = (a & 0x80) > 0
+
+	h.ResultCode = ResultCode(b & 0x0F)
+	h.CheckingDisabled = (b & 0x10) > 0
+	h.AuthedData = (b & 0x20) > 0
+	h.Z = (b & 0x40) > 0
+	h.RecursionAvailable = (b & 0x80) > 0
+
+	questions, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	h.Questions = questions
+
+	answers, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	h.Answers = answers
+
+	authoritativeEntries, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	h.AuthoritativeEntries = authoritativeEntries
+
+	resourceEntries, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	}
+	h.ResourceEntries = resourceEntries
+	
+	return nil
+}
+
+
