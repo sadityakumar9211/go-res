e1a00139e7af771b9eed6e47ea5c5cc334e6da1d
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-09-02 13:02:42 +0530

    ADD: interface and implementation of DNS record for various record types
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -1,13 +1,15 @@
 package dns
 
 import (
+	"errors"
+	"fmt"
 	"net"
 
 	"github.com/sadityakumar9211/go-res/pkg/bytepacketbuffer"
 )
 
 // ResultCode represents DNS result codes.
-type ResultCode int
+type ResultCode uint8
 
 const (
 	NOERROR  ResultCode = 0
@@ -221,6 +223,12 @@ func (q *DnsQuestion) Write(buffer *bytepacketbuffer.BytePacketBuffer) error {
 	return nil
 }
 
+// DnsRecord represents a DNS resource record.
+type DnsRecord interface {
+	Read(buffer *bytepacketbuffer.BytePacketBuffer) error
+	Write(buffer *bytepacketbuffer.BytePacketBuffer) error
+}
+
 // ARecord represents an A DNS record.
 type ARecord struct {
 	Domain string
@@ -230,15 +238,30 @@ type ARecord struct {
 
 // Read reads ARecord data from the buffer.
 func (a *ARecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
+	// Domain Name
 	if err := buffer.ReadQName(&a.Domain); err != nil {
 		return err
 	}
-	ttl, err := buffer.ReadU32()
-	if err != nil {
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
 		return err
 	}
-	a.TTL = ttl
-	buffer.ReadU16() // data length, ignored
+
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		a.TTL = ttl
+	}
+
+	// data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
 	byte1, err := buffer.Read()
 	if err != nil {
 		return err
@@ -261,19 +284,40 @@ func (a *ARecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
 }
 
 // Write writes ARecord data to the buffer.
-func (a *ARecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) error {
-	buffer.WriteQName(a.Domain)
-	buffer.WriteU16(A.QueryTypeToNum()) // for A record the num equivalent is 1
-	buffer.WriteU16(1)                  // class
-	buffer.WriteU32(a.TTL)
-	buffer.WriteU16(4) // data length
-	octets := a.Addr.To4()
-	buffer.WriteU8(octets[0])
-	buffer.WriteU8(octets[1])
-	buffer.WriteU8(octets[2])
-	buffer.WriteU8(octets[3])
+func (a *ARecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	start_pos := buffer.GetPos()
 
-	return nil
+	if err := buffer.WriteQName(a.Domain); err != nil {
+		return 0, err
+	}
+	// for A record the num equivalent is 1
+	if err := buffer.WriteU16(A.QueryTypeToNum()); err != nil {
+		return 0, err
+	} 
+	// class
+	if err := buffer.WriteU16(1); err != nil {
+		return 0, err
+	}       
+
+	if err := buffer.WriteU32(a.TTL); err != nil {
+		return 0, err
+	}
+
+	if err := buffer.WriteU16(4); err != nil {
+		return 0, err
+	} // data length
+
+	octets := a.Addr.To4()
+	var err error
+	err = buffer.WriteU8(octets[0])
+	err = buffer.WriteU8(octets[1])
+	err = buffer.WriteU8(octets[2])
+	err = buffer.WriteU8(octets[3])
+
+	if err != nil {
+		return 0, err
+	}
+	return uint(buffer.GetPos() - start_pos), nil
 }
 
 // NSRecord represents an NS DNS record.
@@ -285,31 +329,66 @@ type NSRecord struct {
 
 // Read reads NSRecord data from the buffer.
 func (n *NSRecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
-	err := buffer.ReadQName(&n.Domain)
-	if err != nil {
+	// Domain Name
+	if err := buffer.ReadQName(&n.Domain); err != nil {
 		return err
 	}
-	n.TTL, err = buffer.ReadU32()
-	if err != nil {
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
 		return err
 	}
-	buffer.ReadU16() // data length, ignored
-	err = buffer.ReadQName(&n.Host)
-	if err != nil {
+
+	if _, err := buffer.ReadU16(); err != nil {
 		return err
 	}
+
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		n.TTL = ttl
+	}
+
+	// data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if err := buffer.ReadQName(&n.Host); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 // Write writes NSRecord data to the buffer.
-func (n *NSRecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) error {
-	buffer.WriteQName(n.Domain)
-	buffer.WriteU16(NS.QueryTypeToNum())
-	buffer.WriteU16(1) // class
-	buffer.WriteU32(n.TTL)
-	buffer.WriteU16(0) // data length
-	buffer.WriteQName(n.Host)
-	return nil
+func (n *NSRecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	start_pos := buffer.GetPos()
+	if err := buffer.WriteQName(n.Domain); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(NS.QueryTypeToNum()); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(1); err != nil {
+		return 0, err
+	} // class
+	if err := buffer.WriteU32(n.TTL); err != nil {
+		return 0, err
+	}
+
+	pos := buffer.GetPos()
+
+	if err := buffer.WriteU16(0); err != nil {
+		return 0, err
+	} // data length
+	if err := buffer.WriteQName(n.Host); err != nil {
+		return 0, err
+	}
+
+	size := buffer.GetPos() - (pos+ 2)
+	buffer.SetU16(pos, uint16(size))
+
+	return uint(buffer.GetPos() - start_pos), nil
 }
 
 // AAAARecord represents an AAAA DNS record.
@@ -321,45 +400,373 @@ type AAAARecord struct {
 
 // Read reads AAAARecord data from the buffer.
 func (a *AAAARecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
-    if err := buffer.ReadQName(&a.Domain); err != nil {
+	// Domain Name
+	if err := buffer.ReadQName(&a.Domain); err != nil {
 		return err
 	}
-    ttl, err := buffer.ReadU32()
-	if err != nil {
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
 		return err
 	}
-	a.TTL = ttl
 
-    buffer.ReadU16() // data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
 
-    // Read the 16 bytes for the IPv6 address
-    ipBytes := make([]byte, 16)
-    for i := 0; i < 16; i++ {
-        val, err := buffer.Read()
-        if err != nil {
-            return err
-        }
-        ipBytes[i] = val
-    }
-    a.Addr = net.IP(ipBytes)
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		a.TTL = ttl
+	}
 
-    return nil
+	// data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	// Read the 16 bytes for the IPv6 address
+	ipBytes := make([]byte, 16)
+	for i := 0; i < 16; i++ {
+		val, err := buffer.Read()
+		if err != nil {
+			return err
+		}
+		ipBytes[i] = val
+	}
+	a.Addr = net.IP(ipBytes)
+
+	return nil
 }
-
 
 // Write writes AAAARecord data to the buffer.
-func (a *AAAARecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) error {
-    buffer.WriteQName(a.Domain)
-    buffer.WriteU16(AAAA.QueryTypeToNum())
-    buffer.WriteU16(1) // class
-    buffer.WriteU32(a.TTL)
-    buffer.WriteU16(16) // data length
+func (a *AAAARecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	start_pos := buffer.GetPos()
+	if err := buffer.WriteQName(a.Domain); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(NS.QueryTypeToNum()); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(1); err != nil {
+		return 0, err
+	} // class
+	if err := buffer.WriteU32(a.TTL); err != nil {
+		return 0, err
+	}
+	buffer.WriteU16(16) // data length
 
-    // Write the 16 bytes for the IPv6 address
-    for _, octet := range a.Addr.To16() {
-        buffer.WriteU8(octet)
-    }
+	// Write the 16 bytes for the IPv6 address
+	for _, octet := range a.Addr.To16() {
+		buffer.WriteU8(octet)
+	}
 
-    return nil
+	return uint(buffer.GetPos() - start_pos), nil 
 }
 
+// AAAARecord represents an AAAA DNS record.
+type MXRecord struct {
+	Domain   string
+	Priority uint16
+	Host     string
+	TTL      uint32
+}
+
+// ReadMXRecord reads MX record data from a byte slice.
+func (m *MXRecord) Read(buffer *bytepacketbuffer.BytePacketBuffer, domain string, ttl uint32) error {
+	// Domain Name
+	if err := buffer.ReadQName(&m.Domain); err != nil {
+		return err
+	}
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		m.TTL = ttl
+	}
+
+	// data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if priority, err := buffer.ReadU16(); err != nil {
+		return err
+	} else {
+		m.Priority = priority
+	}
+
+	if err := buffer.ReadQName(&m.Host); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Write writes MXRecord data to the buffer.
+func (r *MXRecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	start_pos := buffer.GetPos()
+	if err := buffer.WriteQName(r.Domain); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(MX.QueryTypeToNum()); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(1); err != nil {
+		return 0, err
+	} // class
+	if err := buffer.WriteU32(r.TTL); err != nil {
+		return 0, err
+	}
+
+	pos := buffer.GetPos()
+
+	if err := buffer.WriteU16(0); err != nil {
+		return 0, err
+	} 
+
+	// Write the MX priority and host
+	if err := buffer.WriteU16(r.Priority); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteQName(r.Host); err != nil {
+		return 0, err
+	}
+
+	size := uint16(buffer.GetPos() - (pos + 2))
+	if err := buffer.SetU16(pos, size); err != nil {
+		return 0, err
+	}
+
+	return uint(buffer.GetPos()) - uint(start_pos), nil
+}
+
+type CNAMERecord struct {
+	Domain string
+	Host   string
+	TTL    uint32
+}
+
+func (c *CNAMERecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
+	// Domain Name
+	if err := buffer.ReadQName(&c.Domain); err != nil {
+		return err
+	}
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		c.TTL = ttl
+	}
+
+	// data length, ignored
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	} 
+
+	if err := buffer.ReadQName(&c.Host); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Write writes CNameRecord data to the buffer.
+func (c *CNAMERecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	start_pos := buffer.GetPos()
+	if err := buffer.WriteQName(c.Domain); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(CNAME.QueryTypeToNum()); err != nil {
+		return 0, err
+	}
+	if err := buffer.WriteU16(1); err != nil {
+		return 0, err
+	} // class
+	if err := buffer.WriteU32(c.TTL); err != nil {
+		return 0, err
+	}
+
+	
+	pos := buffer.GetPos()
+
+	if err := buffer.WriteU16(0); err != nil {
+		return 0, err
+	} 
+
+	if err := buffer.WriteQName(c.Host); err != nil {
+		return 0, err
+	}
+
+	size := uint16(buffer.GetPos() - (pos + 2))
+	if err := buffer.SetU16(pos, size); err != nil {
+		return 0, err
+	}
+
+	return uint(buffer.GetPos()) - uint(start_pos), nil
+}
+
+type UNKNOWNRecord struct {
+	Domain     string
+	QType      uint16
+	DataLength uint16
+	TTL        uint32
+}
+
+func (u *UNKNOWNRecord) Read(buffer *bytepacketbuffer.BytePacketBuffer) error {
+	// Domain Name
+	if err := buffer.ReadQName(&u.Domain); err != nil {
+		return err
+	}
+	// QueryType
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if _, err := buffer.ReadU16(); err != nil {
+		return err
+	}
+
+	if ttl, err := buffer.ReadU32(); err != nil {
+		return err
+	} else {
+		u.TTL = ttl
+	}
+
+	// data length, ignored
+	dataLength, err := buffer.ReadU16()
+	if err != nil {
+		return err
+	} 
+
+	buffer.Step(int(dataLength))
+
+	return nil
+}
+
+// Write writes UNKNOWNRecord data to the buffer.
+func (u *UNKNOWNRecord) Write(buffer *bytepacketbuffer.BytePacketBuffer) (uint, error) {
+	st := fmt.Sprintf("Skipping record %v", u)
+	fmt.Println(st)
+	return 0, nil
+}
+
+// DnsPacket represents a DNS packet.
+type DnsPacket struct {
+	Header      *DnsHeader
+	Questions   []*DnsQuestion
+	Answers     []DnsRecord
+	Authorities []DnsRecord
+	Resources   []DnsRecord
+}
+
+// NewDnsPacket creates a new DNS packet with default values.
+func NewDnsPacket() *DnsPacket {
+	return &DnsPacket{
+		Header:    NewDnsHeader(),
+		Questions: make([]*DnsQuestion, 0),
+		Answers:   make([]DnsRecord, 0),
+		Authorities: make([]DnsRecord, 0),
+		Resources: make([]DnsRecord, 0),
+	}
+}
+
+// FromBuffer creates a new DNS packet from the buffer.
+func FromBuffer(buffer *bytepacketbuffer.BytePacketBuffer) (*DnsPacket, error) {
+	packet := NewDnsPacket()
+	packet.Header.Read(buffer) // reading the dns packet header
+
+	// Reading questions from the buffer
+	for i := uint16(0); i < packet.Header.Questions; i++ {
+		question := &DnsQuestion{Name: "", QType: UNKNOWN}
+		question.Read(buffer)
+		packet.Questions = append(packet.Questions, question)
+	}
+
+	// Reading answers from the buffer
+	for i := uint16(0); i < packet.Header.Answers; i ++ {
+		var rec DnsRecord
+		if err := rec.Read(buffer); err != nil {
+			return nil, err
+		}
+		packet.Answers = append(packet.Answers, rec)
+	}
+	// Reading authoritative entries from the buffer
+	for i := uint16(0); i < packet.Header.AuthoritativeEntries; i ++ {
+		var rec DnsRecord
+		if err := rec.Read(buffer); err != nil {
+			return nil, err
+		}
+		packet.Authorities = append(packet.Authorities, rec)
+	}
+	// Reading answers from the buffer
+	for i := uint16(0); i < packet.Header.ResourceEntries; i ++ {
+		var rec DnsRecord
+		if err := rec.Read(buffer); err != nil {
+			return nil, err
+		}
+		packet.Resources = append(packet.Resources, rec)
+	}
+
+	return packet, nil
+}
+
+
+// Write writes the DNS packet to the buffer.
+func (p *DnsPacket) Write(buffer *bytepacketbuffer.BytePacketBuffer) error {
+	p.Header.Questions = uint16(len(p.Questions))
+	p.Header.Answers = uint16(len(p.Answers))
+	p.Header.AuthoritativeEntries = uint16(len(p.Authorities))
+	p.Header.ResourceEntries = uint16(len(p.Resources))
+
+	p.Header.Write(buffer)
+
+	for _, question := range p.Questions {
+		question.Write(buffer)
+	}
+
+	return nil
+}
+
+// GetRandomA returns a random A record from the packet.
+func (p *DnsPacket) GetRandomA() (net.IP, error) {
+	for _, answer := range p.Answers {
+		if aRecord, ok := answer.(*ARecord); ok {
+			return aRecord.Addr, nil
+		}
+	}
+
+	return nil, errors.New("No A record found in answers")
+}
+
+// GetResolvedNS returns the resolved IP address for an NS record if possible.
+func (p *DnsPacket) GetResolvedNS(qname string) (net.IP, error) {
+	for _, authority := range p.Authorities {
+		if nsRecord, ok := authority.(*NSRecord); ok && nsRecord.Domain == qname {
+			ns := nsRecord.Host
+			for _, resource := range p.Resources {
+				if aRecord, ok := resource.(*ARecord); ok && aRecord.Domain == ns {
+					return aRecord.Addr, nil
+				}
+			}
+			return nil, errors.New("No matching A record found for the NS record")
+		}
+	}
+
+	return nil, errors.New("No NS record found in authorities")
+}
